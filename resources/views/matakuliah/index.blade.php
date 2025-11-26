275becd6bbb25ff92c41f41835cf9fb4c10ffa4f
mirzayogy <mirza.yogy@gmail.com>
2025-11-26 11:51:26 +0800

    templating table

--- resources/views/matakuliah/index.blade.php
+++ resources/views/matakuliah/index.blade.php
@@ -1,62 +1,72 @@
-<style>
-    table {
-        font-family: arial, sans-serif;
-        border-collapse: collapse;
-        width: 100%;
-    }
+@extends('template.default')
+@section('content')
+    @if (session('berhasil'))
+        {{ session('berhasil') }}
+        <br>
+    @endif
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Matakuliah</h1>
+        <a href="{{ route('matakuliah.create') }}" class="d-none d-sm-inline-block btn btn-sm btn-success shadow-sm">
+            <i class="fas fa-plus fa-sm text-white-50"></i> Tambah
+        </a>
+    </div>
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Data Matakuliah</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <th>ID</th>
+                        <th>Kode MK</th>
+                        <th>Nama</th>
+                        <th>Jumlah SKS</th>
+                        <th>Prodi</th>
+                        <th>Opsi</th>
+                    </thead>
+                    <tbody>
+                        @foreach ($banyak_matakuliah as $matakuliah)
+                            <tr>
+                                <td>{{ $matakuliah['id'] }}</td>
+                                <td>{{ $matakuliah['kode_mk'] }}</td>
+                                <td>{{ $matakuliah['nama'] }}</td>
+                                <td>{{ $matakuliah['jumlah_sks'] }}</td>
+                                <td>{{ $matakuliah['prodi_id'] }}</td>
+                                <td style="align-items: center">
+                                    <a href="{{ route('matakuliah.edit', $matakuliah) }}" class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></a>
+                                    <button class="btn btn-danger btn-sm"
+                                        onclick="konfirmasi('{{ route('matakuliah.destroy', $matakuliah) }}')"><i class="fa fa-trash"></i></button>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <form action="" method="POST" id="deleteForm">
+        @csrf
+        @method('DELETE')
 
-    td,
-    th {
-        border: 1px solid #dddddd;
-        text-align: left;
-        padding: 8px;
-    }
+    </form>
 
-    tr:nth-child(even) {
-        background-color: #dddddd;
-    }
-</style>
-@if (session('berhasil'))
-    {{ session('berhasil') }}
-    <br>
-@endif
-<a href="{{ route('matakuliah.create') }}">Tambah</a>
-<table>
-    <thead>
-        <th>ID</th>
-        <th>Kode MK</th>
-        <th>Nama</th>
-        <th>Jumlah SKS</th>
-        <th>Prodi</th>
-        <th>Opsi</th>
-    </thead>
-    <tbody>
-        @foreach ($banyak_matakuliah as $matakuliah)
-            <tr>
-                <td>{{ $matakuliah['id'] }}</td>
-                <td>{{ $matakuliah['kode_mk'] }}</td>
-                <td>{{ $matakuliah['nama'] }}</td>
-                <td>{{ $matakuliah['jumlah_sks'] }}</td>
-                <td>{{ $matakuliah['prodi_id'] }}</td>
-                <td>
-                    <a href="{{ route('matakuliah.edit', $matakuliah) }}">Ubah</a>
-                    <button onclick="konfirmasi('{{ route('matakuliah.destroy', $matakuliah) }}')">Hapus</button>
-                </td>
-            </tr>
-        @endforeach
-    </tbody>
-</table>
-<form action="" method="POST" id="deleteForm">
-    @csrf
-    @method('DELETE')
-
-</form>
-
-<script>
-    function konfirmasi(href) {
-        if (confirm('Hapus?')) {
-            document.getElementById('deleteForm').action = href
-            document.getElementById('deleteForm').submit()
+    <script>
+        function konfirmasi(href) {
+            if (confirm('Hapus?')) {
+                document.getElementById('deleteForm').action = href
+                document.getElementById('deleteForm').submit()
+            }
         }
-    }
-</script>
+    </script>
+@endsection
+
+@push('styles')
+    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet">
+@endpush
+
+@push('scripts')
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('js/demo/datatables-demo.js') }}"></script>
+@endpush
